b105d7ae3ee421ca519b29ca10511c86a14b54d8
PelayoIglesiass <uo266600@uniovi.es>
2025-12-24 20:48:28 +0100

    Base inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500 hover:text-blue-400 cursor-pointer">
+        K-Board en línea 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
